cb3671a6cd60437a325685df4c5866f08ade80e1
RomainVasseur87-fr <romain.vasseur87@gmail.com>
2021-03-08 17:01:20 +0100

    increment dao

--- autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/AdresseDaoJpa.java
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/AdresseDaoJpa.java
@@ -2,6 +2,11 @@ package m2i.formation.dao.jpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import m2i.formation.Application;
 import m2i.formation.dao.IAdresseDao;
 import m2i.formation.model.Adresse;
 
@@ -9,32 +14,140 @@ public class AdresseDaoJpa implements IAdresseDao {
 
 	@Override
 	public List<Adresse> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		List<Adresse> adresses = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Adresse> myQuery = em.createQuery("select adr from Adresse adr", Adresse.class);
+			adresses = myQuery.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return adresses;
 	}
 
 	@Override
 	public Adresse find(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Adresse adresse = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			adresse = em.find(Adresse.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return adresse;
 	}
 
 	@Override
 	public void create(Adresse obj) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.persist(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 	@Override
 	public Adresse update(Adresse obj) {
-		// TODO Auto-generated method stub
-		return null;
+		Adresse adresse = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			adresse = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return adresse;
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			Adresse adresse = em.find(Adresse.class, id);
+			em.remove(adresse);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 }
--- autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/ArticleDaoJpa.java
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/ArticleDaoJpa.java
@@ -2,6 +2,11 @@ package m2i.formation.dao.jpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import m2i.formation.Application;
 import m2i.formation.dao.IArticleDao;
 import m2i.formation.model.Article;
 
@@ -9,32 +14,139 @@ public class ArticleDaoJpa implements IArticleDao {
 
 	@Override
 	public List<Article> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		List<Article> articles = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Article> myQuery = em.createQuery("select art from Article art", Article.class);
+			articles = myQuery.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return articles;
 	}
 
 	@Override
 	public Article find(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Article article = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			article = em.find(Article.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		return article;
 	}
 
 	@Override
 	public void create(Article obj) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.persist(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 	@Override
 	public Article update(Article obj) {
-		// TODO Auto-generated method stub
-		return null;
+		Article article = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			article = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return article;
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			Article article = em.find(Article.class, id);
+			em.remove(article);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 }
