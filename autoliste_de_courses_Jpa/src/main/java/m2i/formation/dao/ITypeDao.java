94a7dac0a6a678c8f9153d6431b2939bf3ad4642
Varlet Christophe <varletchristophe@gmail.com>
2021-03-08 15:09:24 +0100

    Interfaces DAO Droit, Type et Utilisateur

---
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/ITypeDao.java
@@ -0,0 +1,7 @@
+package m2i.formation.dao;
+
+import m2i.formation.model.Type;
+
+public interface ITypeDao extends IDao<Type, Long>{
+
+}
---
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/IUtilisateurDao.java
@@ -0,0 +1,7 @@
+package m2i.formation.dao;
+
+import m2i.formation.model.Utilisateur;
+
+public interface IUtilisateurDao extends IDao<Utilisateur, Long> {
+
+}
