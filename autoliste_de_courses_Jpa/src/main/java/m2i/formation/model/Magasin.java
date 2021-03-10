403f9b8c342862ccb2e96602ab81a82e7327f3cb
RomainVasseur87-fr <romain.vasseur87@gmail.com>
2021-03-10 15:46:30 +0100

    correction erreur fausse manip

--- autoliste_de_courses_Jpa/src/main/java/m2i/formation/model/Magasin.java
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/model/Magasin.java
@@ -3,15 +3,20 @@ package m2i.formation.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Transient;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Version;
 
 @Entity
 public class Magasin {
 	@Id
 	@GeneratedValue
 	private long id;
+	@Version
+	private int version;
 	private String nom;
-	@Transient
+	@OneToOne
+	@JoinColumn (name = "adresse_id")
 	private Adresse adresse;
 	
 	public Magasin() {
