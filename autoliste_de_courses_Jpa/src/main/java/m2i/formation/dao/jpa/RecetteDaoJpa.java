fd64994661b3e85cc1896357f18ce371bfccfa6c
DJAMohamed <djadane@gmail.com>
2021-03-10 14:44:20 +0100

    MAJ JPA DAO de : catégorie, process, recette et thème.

--- autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/RecetteDaoJpa.java
+++ autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/jpa/RecetteDaoJpa.java
@@ -1,134 +1,50 @@
 package m2i.formation.dao.jpa;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
-import m2i.formation.Application;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import m2i.formation.dao.IRecetteDao;
 import m2i.formation.model.Recette;
 
+@Repository
+@Transactional
 public class RecetteDaoJpa implements IRecetteDao {
+	
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public List<Recette> findAll() {
-		List<Recette> recettes = new ArrayList<Recette>();
-		EntityManager em = null;
-		EntityTransaction tx = null;
-		try {
-			em = Application.getInstance().getEmf().createEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
 			TypedQuery<Recette> query = em.createQuery("select r from Recette r", Recette.class);
-			recettes = query.getResultList();
-			tx.commit();
-		} catch (Exception e) {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
-		return recettes;
+			return query.getResultList();
 	}
 
 	@Override
 	public Recette find(Long id) {
-		Recette recette = null;
-		EntityManager em = null;
-		EntityTransaction tx = null;
-		try {
-			em = Application.getInstance().getEmf().createEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
-			recette = em.find(Recette.class, id);
-			tx.commit();
-		} catch (Exception e) {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
-		return recette;
+		return em.find(Recette.class, id);
 	}
 
 	@Override
 	public void create(Recette obj) {
-		EntityManager em = null;
-		EntityTransaction tx = null;
-		try {
-			em = Application.getInstance().getEmf().createEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
-			em.persist(obj);
-			tx.commit();
-		} catch (Exception e) {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
+		em.persist(obj);
 	}
 
 	@Override
 	public Recette update(Recette obj) {
-		Recette recette = null;
-		EntityManager em = null;
-		EntityTransaction tx = null;
-		try {
-			em = Application.getInstance().getEmf().createEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
-			recette = em.merge(obj);
-			tx.commit();
-		} catch (Exception e) {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
+		Recette recette = em.merge(obj);
 		return recette;
 	}
 
 	@Override
 	public void delete(Long id) {
-		EntityManager em = null;
-		EntityTransaction tx = null;
-		try {
-			em = Application.getInstance().getEmf().createEntityManager();
-			tx = em.getTransaction();
-			tx.begin();
-			Recette recette = em.find(Recette.class, id);
-			em.remove(recette);
-			tx.commit();
-		} catch (Exception e) {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}		
+		Recette recette = em.find(Recette.class, id);
+		em.remove(recette);
 	}
 	
 }
