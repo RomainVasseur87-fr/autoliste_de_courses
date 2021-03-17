4643c7c46f0a9f08a642debf20fd7a4b3559fc77
Varlet Christophe <varletchristophe@gmail.com>
2021-03-17 10:09:04 +0100

    Test

--- autoliste-boot-back/src/main/java/m2i/formation/dao/IAdresseDao.java
+++ autoliste-boot-back/src/main/java/m2i/formation/dao/IAdresseDao.java
@@ -7,16 +7,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import m2i.formation.model.Adresse;
+import m2i.formation.model.Utilisateur;
 
 public interface IAdresseDao extends JpaRepository<Adresse, Long> {
 	
 	@Query("select a from adresse where a.ville =:ville")
 	List<Adresse> findByVille(@Param("ville")String ville);
 	
+<<<<<<< Updated upstream
 	@Query("select a from adresse where a.codePostal = :codePostal")
 	List<Adresse> findByCodePostal(@Param("codePostal")String codePostal);
 
 	@Query("select u.adresse from Utilisateur u where u.id = :id")
 	Adresse findByUtilisateur(@Param("id") long id);
 
+=======
+	List<Adresse> findByVille(String ville);
+	
+	List<Adresse> findByCodePostal(String codePostal);
+	
+	@Query("select u.utilisateur from Type t where t.id = :id")
+	Utilisateur findByType (@Param("id") long id);
+	
+>>>>>>> Stashed changes
 }
--- autoliste-boot-back/src/main/java/m2i/formation/dao/IDroitDao.java
+++ autoliste-boot-back/src/main/java/m2i/formation/dao/IDroitDao.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import m2i.formation.model.Droit;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 public interface IDroitDao extends JpaRepository<Droit, Long> {
 	
