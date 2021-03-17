ac0b962f5157aecfdc5af555248b4c22aab676cb
Varlet Christophe <varletchristophe@gmail.com>
2021-03-17 12:04:26 +0100

    Auto stash before merge of "main" and "origin/main"

--- autoliste-boot-back/src/main/java/m2i/formation/dao/IAdresseDao.java
+++ autoliste-boot-back/src/main/java/m2i/formation/dao/IAdresseDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import m2i.formation.model.Adresse;
-import m2i.formation.model.Utilisateur;
+
 
 public interface IAdresseDao extends JpaRepository<Adresse, Long> {
 	
@@ -19,14 +19,5 @@ public interface IAdresseDao extends JpaRepository<Adresse, Long> {
 
 	@Query("select u.adresse from Utilisateur u where u.id = :id")
 	Adresse findByUtilisateur(@Param("id") long id);
-
-=======
-	List<Adresse> findByVille(String ville);
 	
-	List<Adresse> findByCodePostal(String codePostal);
-	
-	@Query("select u.utilisateur from Type t where t.id = :id")
-	Utilisateur findByType (@Param("id") long id);
-	
->>>>>>> Stashed changes
 }
--- autoliste-boot-back/src/main/java/m2i/formation/dao/IDroitDao.java
+++ autoliste-boot-back/src/main/java/m2i/formation/dao/IDroitDao.java
@@ -3,12 +3,7 @@ package m2i.formation.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import m2i.formation.model.Droit;
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 public interface IDroitDao extends JpaRepository<Droit, Long> {
 	
