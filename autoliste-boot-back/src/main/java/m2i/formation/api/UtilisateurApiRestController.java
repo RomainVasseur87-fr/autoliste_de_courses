3495d55f9c8326f04b5ada4c8cf32d51efa5fe61
Varlet Christophe <varletchristophe@gmail.com>
2021-03-19 16:43:13 +0100

    Commit finalisation utilisateurs/roles

--- autoliste-boot-back/src/main/java/m2i/formation/api/UtilisateurApiRestController.java
+++ autoliste-boot-back/src/main/java/m2i/formation/api/UtilisateurApiRestController.java
@@ -1,13 +1,17 @@
 package m2i.formation.api;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,7 +42,7 @@ public class UtilisateurApiRestController {
 
 	@GetMapping("/{id}")
 	@JsonView(IViews.IViewUtilisateur.class)
-	public Utilisateur find(@PathVariable Long id) {
+	public Utilisateur find(@PathVariable String id) {
 		Optional<Utilisateur> optUtilisateur = utilisateurDao.findById(id);
 
 		if (optUtilisateur.isPresent()) {
@@ -50,7 +54,7 @@ public class UtilisateurApiRestController {
 	
 	@GetMapping("/{id}/detail")
 	@JsonView(IViews.IViewUtilisateurDetail.class)
-	public Utilisateur detail(@PathVariable Long id) {
+	public Utilisateur detail(@PathVariable String id) {
 		Optional<Utilisateur> optUtilisateur = utilisateurDao.findById(id);
 
 		if (optUtilisateur.isPresent()) {
@@ -60,7 +64,6 @@ public class UtilisateurApiRestController {
 		}
 	}
 	
-
 	@PostMapping("")
 	@JsonView(IViews.IViewUtilisateur.class)
 	@PreAuthorize("hasRole('ADMIN')")
@@ -68,6 +71,15 @@ public class UtilisateurApiRestController {
 		utilisateur = utilisateurDao.save(utilisateur);
 
 		return utilisateur;
+	}
+
+	@PostMapping("/liste")
+	@JsonView(IViews.IViewUtilisateur.class)
+	@PreAuthorize("hasRole('ADMIN')")
+	public List<Utilisateur> create(@RequestBody List<Utilisateur> utilisateurs) {
+		utilisateurs = utilisateurDao.saveAll(utilisateurs);
+
+		return utilisateurs;
 	}
 
 	@PutMapping("/{username}")
@@ -82,10 +94,30 @@ public class UtilisateurApiRestController {
 
 		return utilisateur;
 	}
+	
+	@PatchMapping("/{id}")
+	@JsonView(IViews.IViewUtilisateur.class)
+	public Utilisateur partialUpdate(@RequestBody Map<String, Object> updates, @PathVariable String id) {
+		if (!utilisateurDao.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+		}
+
+		final Utilisateur utilisateurFind = utilisateurDao.findById(id).get();
+
+		updates.forEach((key, value) -> {
+			Field field = ReflectionUtils.findField(Utilisateur.class, key);
+			ReflectionUtils.makeAccessible(field);
+			ReflectionUtils.setField(field, utilisateurFind, value);
+		});
+
+		Utilisateur utilisateurUpdate = utilisateurDao.save(utilisateurFind);
+
+		return utilisateurUpdate;
+	}
 
 	@DeleteMapping("/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
-	public void delete(@PathVariable Long id) {
+	public void delete(@PathVariable String id) {
 		if (!utilisateurDao.existsById(id)) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
 		}
--- autoliste-boot-back/src/main/java/m2i/formation/dao/IUtilisateurDao.java
+++ autoliste-boot-back/src/main/java/m2i/formation/dao/IUtilisateurDao.java
@@ -1,5 +1,6 @@
 package m2i.formation.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,11 +9,18 @@ import org.springframework.data.repository.query.Param;
 
 import m2i.formation.model.Utilisateur;
 
-public interface IUtilisateurDao extends JpaRepository<Utilisateur, Long> {
+public interface IUtilisateurDao extends JpaRepository<Utilisateur, String> {
+	
+	
+	@Query("select u from Utilisateur u where u.username =:username")
+	List<Utilisateur> findByUsername(@Param("username") String username);
+	
+	@Query("select u from Utilisateur u where u.enable =:enable")
+	List<Utilisateur> findByEnable(@Param("enable") boolean enable);
 	
 	@Query("select u from Utilisateur u left join fetch u.roles where u.username=:username")
 	public Optional<Utilisateur> findByIdWithRoles(@Param("username") String username);
-
+	
 	public boolean existsByUsername(String username);
 	
 }
