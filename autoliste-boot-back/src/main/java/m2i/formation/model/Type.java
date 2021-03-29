b5178707a0c6a3bfae576c22bc2545e00b39957d
RomainVasseur87-fr <romain.vasseur87@gmail.com>
2021-03-29 10:16:43 +0200

    suppression securité cause conflit corps avec front

--- autoliste-boot-back/src/main/java/m2i/formation/model/Type.java
+++ autoliste-boot-back/src/main/java/m2i/formation/model/Type.java
@@ -8,9 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Version;
@@ -25,82 +22,54 @@ public class Type {
 	private int version;
 	@Column(name = "nom", length = 255)
 	private String nom;
-	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "type_droits", joinColumns = @JoinColumn(name = "type_id"), inverseJoinColumns = @JoinColumn(name = "droit_id"))
-	private List<Droit> droits = new ArrayList<Droit>();
 	@OneToMany(mappedBy = "type", fetch = FetchType.LAZY)
 	private List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
-	
-	
+
 	public Type() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	
-	public Type(String nom, List<Droit> droits) {
+	public Type(String nom) {
 		super();
 		this.nom = nom;
-		this.droits = droits;
 	}
 
-
-	public Type(Long id, String nom, List<Droit> droits) {
+	public Type(Long id, String nom) {
 		super();
 		this.id = id;
 		this.nom = nom;
-		this.droits = droits;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public int getVersion() {
 		return version;
 	}
 
-
 	public void setVersion(int version) {
 		this.version = version;
 	}
-
 
 	public String getNom() {
 		return nom;
 	}
 
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-
-	public List<Droit> getDroits() {
-		return droits;
-	}
-
-
-	public void setDroits(List<Droit> droits) {
-		this.droits = droits;
-	}
-
 
 	public List<Utilisateur> getUtilisateurs() {
 		return utilisateurs;
 	}
 
-
 	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
 		this.utilisateurs = utilisateurs;
 	}
 
-	
 }
--- autoliste_de_courses_Jpa/src/main/java/m2i/formation/dao/IProduitDao.java
+++
@@ -1,9 +0,0 @@
-package m2i.formation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import m2i.formation.model.Produit;
-
-public interface IProduitDao extends JpaRepository<Produit, Long>{
-
-}
